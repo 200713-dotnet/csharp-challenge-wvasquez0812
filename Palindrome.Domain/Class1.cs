612b9d790bcfa4079493f9483acd27d7d9a60dfa
winston vasquez <wvasquez@villanova.edu>
2020-07-20 16:53:52 -0400

    'Palindrome intial code'

--- Palindrome.Domain/Class1.cs
+++ Palindrome.Domain/Class1.cs
@@ -4,5 +4,37 @@ namespace Palindrome.Domain
 {
     public class Class1
     {
+        public static void  Main()
+        {
+            System.Console.WriteLine("Welcome to Palindrome Checker. Enter word to be checked: ");
+            var word = System.Console.ReadLine().ToLower();
+            
+            int leftIndex = 0;
+            int rightIndex = word.Length - 1;
+            
+        
+        }
+
+        public bool isPalindrome(string word, int leftIndex, int rightIndex)
+        {
+            bool isPalindrome;
+            bool stay = true;
+
+            while(stay)
+            {
+                if(leftIndex < rightIndex)
+                {
+                    if(word[leftIndex] != word[rightIndex])
+                    {
+                        stay = false;
+                        isPalindrome =false;   
+                    }
+                }
+                else
+                {
+                    stay = false;
+                }
+            }
+        }
     }
 }
